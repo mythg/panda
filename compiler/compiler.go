45810aa30e299f1992c4a3b06ae93868438cac41
zhihao.guo <zhihao.guo@advancegroup.com>
2024-04-05 20:52:12 +0800

    add compiler

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,10 +1,35 @@
 package compiler
 
 import (
+	"panda/ast"
 	"panda/code"
+	"panda/object"
 )
 
 type Compiler struct {
 	instructions code.Instructions
 	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
 }
