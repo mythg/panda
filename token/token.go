a86e09bb097d69d58093cb3eccf48b4e25584b98
zhihao.guo <zhihao.guo@advancegroup.com>
2024-04-07 15:34:22 +0800

    [commit title] : add token and lexer [module name] : [modify description] : [relate on jira/confluence/bug] :

--- token/token.go
+++ token/token.go
@@ -1,1 +1,40 @@
 package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	IDENT   = "IDENT"
+	INT     = "INT" // 1343456
+
+	ASSIGN    = "="
+	PLUS      = "+"
+	MINUS     = "-"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
