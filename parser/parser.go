64d9cc998fe8ecc342147096bfd3b700c1422c16
zhihao.guo <zhihao.guo@advancegroup.com>
2024-04-07 20:22:23 +0800

    [commit title] : add parse return statement [module name] : [modify description] : [relate on jira/confluence/bug] :

--- parser/parser.go
+++ parser/parser.go
@@ -60,6 +60,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -79,6 +81,16 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
 
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,7 +9,7 @@ import (
 func TestLetStatements(t *testing.T) {
 	input := `
 	let x = 5;
-	let y = 10;
+	let y 10;
 	let floor = 838 383;
 `
 
@@ -17,6 +17,8 @@ func TestLetStatements(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil.")
 	}
@@ -38,7 +40,34 @@ func TestLetStatements(t *testing.T) {
 		}
 	}
 
+}
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993 322;
+`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
 	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got '%q'", returnStmt.TokenLiteral())
+		}
+	}
 }
 
 func checkParserErrors(t *testing.T, p *Parser) {
